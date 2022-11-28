db248f92323f5f15ec4e43b051209ef22974bab9
WGB5445 <919603023@qq.com>
2022-11-28 19:00:29 +0800

    fix starcoin airdrop

--- routes/projectadmin.js
+++ routes/projectadmin.js
@@ -67,8 +67,81 @@ router.post('/', async function (req, res, next) {
                 airdrops.push(new CoinAirdropStrcoin(
                     v.at(0),
                     Number(v[1]) * Math.pow(10, Number(body.data.token_precision)),
-                    body.data.token
+                    i
                 ))
+            });
+            let root = '';
+            let airdrop_id = await db.sequelize.transaction(async (transaction) => {
+                let project = await Airdrop_projects.create(
+                    {
+                        id: null,
+                        project: body.data.project,
+                        name: body.data.name,
+                        name_en: body.data.name_en,
+                        token: body.data.token,
+                        token_icon: "",
+                        token_symbol: body.data.token_symbol,
+                        token_precision: body.data.token_precision,
+                        total_amount: 0,
+                        valid_amount: 0,
+                        start_at: new Date(),
+                        end_at: new Date().setDate(new Date().getDate() + 15),
+                        owner_address: owner_address,
+                        root: "",
+                        network_version: body.data.chainid,
+                        chain: body.data.chain,
+                        create_at: new Date(),
+                        update_at: new Date()
+                    }
+                    , {transaction: transaction});
+
+                let leaves = airdrops.map(x => sha3_256.sha3_256(x.serialize(project.id)));
+                let tree = new MerkleTree(leaves, sha3_256.sha3_256, {sortPairs: true});
+                root = tree.getRoot().toString('hex');
+                console.log(root)
+                airdrops = airdrops.map(x => {
+                    return {
+                        address: x.address_string(),
+                        amount: x.get_amount(),
+                        coin_type: body.data.token,
+                        proof: tree.getProof(sha3_256.sha3_256(x.serialize(project.id))).map(x => {
+                            return Buffer.from(x.data).toString('hex')
+                        })
+                    }
+                });
+                let i = 0;
+                for (let value of airdrops) {
+
+                    await Airdrop_records.create(
+                        {
+                            id: null,
+                            address: value.address,
+                            amount: value.amount,
+                            idx: i,
+                            proof: JSON.stringify(value.proof),
+                            airdrop_id: project.id,
+                            status: '0',
+                            created_at: new Date(),
+                            updated_at: new Date()
+                        }, {transaction: transaction}
+                    )
+                    i = i + 1
+                }
+
+                await db.sequelize.query(`UPDATE airdrop_projects
+                                          set root = "0x${root}"
+                                          where id = ${project.id}`, {transaction: transaction});
+                return project.id
+            })
+
+            res.send({
+                error: 200,
+                data: {
+                    airdrop_id: airdrop_id,
+                    length: airdrops.length,
+                    total: total.toString(),
+                    root: `0x${root}`,
+                }
 
             });
         } else {
@@ -80,79 +153,79 @@ router.post('/', async function (req, res, next) {
                     body.data.token
                 ))
             });
+            let leaves = airdrops.map(x => sha3_256.sha3_256(x.serialize()));
+            let tree = new MerkleTree(leaves, sha3_256.sha3_256, {sortPairs: true});
+            let root = tree.getRoot().toString('hex');
+
+            airdrops = airdrops.map(x => {
+                return {
+                    address: x.address_string(),
+                    amount: x.get_amount(),
+                    coin_type: x.get_coin_type(),
+                    proof: tree.getProof(sha3_256.sha3_256(x.serialize())).map(x => {
+                        return Buffer.from(x.data).toString('hex')
+                    })
+                }
+            });
+
+            let airdrop_id = await db.sequelize.transaction(async (transaction) => {
+                let project = await Airdrop_projects.create(
+                    {
+                        id: null,
+                        project: body.data.project,
+                        name: body.data.name,
+                        name_en: body.data.name_en,
+                        token: body.data.token,
+                        token_icon: "",
+                        token_symbol: body.data.token_symbol,
+                        token_precision: body.data.token_precision,
+                        total_amount: 0,
+                        valid_amount: 0,
+                        start_at: new Date(),
+                        end_at: new Date().setDate(new Date().getDate() + 15),
+                        owner_address: owner_address,
+                        root: root,
+                        network_version: body.data.chainid,
+                        chain: body.data.chain,
+                        create_at: new Date(),
+                        update_at: new Date()
+                    }
+                    , {transaction: transaction});
+
+                let i = 0;
+                for (let value of airdrops) {
+
+                    await Airdrop_records.create(
+                        {
+                            id: null,
+                            address: value.address,
+                            amount: value.amount,
+                            idx: i,
+                            proof: JSON.stringify(value.proof),
+                            airdrop_id: project.id,
+                            status: '0',
+                            created_at: new Date(),
+                            updated_at: new Date()
+                        }, {transaction: transaction}
+                    )
+                    i = i + 1
+                }
+                return project.id
+            })
+
+            res.send({
+                error: 200,
+                data: {
+                    airdrop_id: airdrop_id,
+                    length: airdrops.length,
+                    total: total.toString(),
+                    root: root,
+                }
+
+            });
         }
 
 
-        let leaves = airdrops.map(x => sha3_256.sha3_256(x.serialize()));
-        let tree = new MerkleTree(leaves, sha3_256.sha3_256, {sortPairs: true});
-        let root = tree.getRoot().toString('hex');
-
-        airdrops = airdrops.map(x => {
-            return {
-                address: x.address_string(),
-                amount: x.get_amount(),
-                coin_type: x.get_coin_type(),
-                proof: tree.getProof(sha3_256.sha3_256(x.serialize())).map(x => {
-                    return Buffer.from(x.data).toString('hex')
-                })
-            }
-        });
-
-        let airdrop_id = await db.sequelize.transaction(async (transaction) => {
-            let project = await Airdrop_projects.create(
-                {
-                    id: null,
-                    project: body.data.project,
-                    name: body.data.name,
-                    name_en: body.data.name_en,
-                    token: body.data.token,
-                    token_icon: "",
-                    token_symbol: body.data.token_symbol,
-                    token_precision: body.data.token_precision,
-                    total_amount: 0,
-                    valid_amount: 0,
-                    start_at: new Date(),
-                    end_at: new Date().setDate(new Date().getDate() + 15),
-                    owner_address: owner_address,
-                    root: root,
-                    network_version: body.data.chainid,
-                    chain: body.data.chain,
-                    create_at: new Date(),
-                    update_at: new Date()
-                }
-                , {transaction: transaction});
-
-            let i = 0;
-            for (let value of airdrops) {
-
-                await Airdrop_records.create(
-                    {
-                        id: null,
-                        address: value.address,
-                        amount: value.amount,
-                        idx: i,
-                        proof: JSON.stringify(value.proof),
-                        airdrop_id: project.id,
-                        status: '0',
-                        created_at: new Date(),
-                        updated_at: new Date()
-                    }, {transaction: transaction}
-                )
-                i = i + 1
-            }
-            return project.id
-        })
-
-        res.send({
-            error: 200,
-            data: {
-                airdrop_id: airdrop_id,
-                length: airdrops.length,
-                total: total.toString(),
-                root: root,
-            }
-
-        });
     } catch (e) {
         res.send({
             error: 400,
